dc83ef901aa38de8fb9089ce3911ee0e3c6203fe
Nicolas Oleinizak <nicolas.oleinizak@gmail.com>
2022-12-23 09:08:31 -0300

    add mnist training

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import tensorflow as tf
 import pandas as pd
 
+SHUFFLE_BUFFER = 500
+BATCH_SIZE = 2
 csv_file = tf.keras.utils.get_file('heart.csv', 'https://storage.googleapis.com/download.tensorflow.org/data/heart.csv')
 
 df = pd.read_csv(csv_file)
 print(df.head())
+
+numeric_feature_names = ['age', 'thalach', 'trestbps', 'chol', 'oldpeak']
+numeric_features = df[numeric_feature_names]
+print(numeric_features.head())
+
+tensor = tf.convert_to_tensor(numeric_features)
+print(tensor)
